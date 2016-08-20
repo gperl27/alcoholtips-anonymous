1cf72ec1cc91290f8aae1fc0330878aca0af4c3c
Greg Perlman <gperlman27@gmail.com>
2016-08-20 17:30:33 -0400

    Configure database for production

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160814130855) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "drinks", force: :cascade do |t|
     t.string   "name"
     t.text     "ingredients"
