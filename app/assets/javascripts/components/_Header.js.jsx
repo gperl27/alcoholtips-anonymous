601c50635714480fc3df44daca4f3d480bfe05ab
Greg Perlman <gperlman27@gmail.com>
2016-08-30 09:19:37 -0400

    Fix header text

--- app/assets/javascripts/components/_Header.js.jsx
+++ app/assets/javascripts/components/_Header.js.jsx
@@ -5,7 +5,7 @@ const Header = React.createClass({
 				<h1>Alcoholtips Anonymous</h1>
 				<div className='text'>
 					<p>Alcoholtips Anonymous is a web application for drinkers wordlwide.</p>
-					<p>Add a special drink you've had or make with a tip or story that goes along with it by clicking the 'New Tip' button.</p>
+					<p>Add a special drink you've had or made with a tip or story that goes along with it by clicking the 'New Tip' button.</p>
 				</div>
 			</div>	
 		)
